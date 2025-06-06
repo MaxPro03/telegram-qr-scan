09520f982a603657892511818abfd1146a765136
unknown <pumpkinmaxpro@gmail.com>
2025-06-06 17:53:42 +0500

    delete no needed packeges

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,15 @@
-import { fileURLToPath, URL } from "node:url"
+import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from "vite"
-import vue from "@vitejs/plugin-vue"
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: '/',
   plugins: [vue()],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  // server: {
-  //   proxy:
-  //     mode === "development"
-  //       ? {
-  //           "/api": {
-  //             target: "https://admin.gogomarket.uz", // Целевой сервер
-  //             changeOrigin: true, // Изменяет Origin для обхода CORS
-  //             rewrite: (path) => path.replace(/^\/api/, ""), // Убираем /api
-  //           },
-  //         }
-  //       : undefined, // В продакшене прокси не используется
-  // },
-  // server: {
-  //   proxy: {
-  //     "/api": {
-  //       target: "https://admin.gogomarket.uz",
-  //       changeOrigin: true,
-  //       rewrite: (path) => path.replace(/^\/api/, "/api/v2"),
-  //     },
-  //   },
-  // },
 })
