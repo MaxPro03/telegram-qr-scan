22e1a580f1a92d680c91f67a262100741346e2e2
unknown <pumpkinmaxpro@gmail.com>
2025-06-07 02:16:08 +0500

    feat: change logic and response

--- src/views/QrScanTestView.vue
+++ src/views/QrScanTestView.vue
@@ -31,10 +31,12 @@ async function showScanQrPopup(linksOnly) {
             },
           })
 
+          const result = await response.json()
+
           if (!response.ok) {
-            tg.showAlert(response.data.message)
+            tg.showAlert(result.message)
           }
-          tg.showAlert(response.data.message)
+          tg.showAlert(result.message)
           return true
         } catch (error) {
           tg.showAlert(error.message)
@@ -44,37 +46,14 @@ async function showScanQrPopup(linksOnly) {
   )
 }
 
-const sendSomeCode = async () => {
-  try {
-    const response = await fetch(`https://tl.gogomarket.uz/telegram-admin-bot/check-registration`, {
-      method: 'POST',
-      body: JSON.stringify({
-        qrCode: 213,
-      }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-
-    if (!response.ok) {
-      tg.showAlert(response.data.message)
-    }
-    tg.showAlert(response.data.message)
-    console.log(response)
-    return true
-  } catch (error) {
-    tg.showAlert(error.message)
-  }
-}
 onMounted(() => {
   tg.ready()
-  sendSomeCode()
 })
 </script>
 
 <style scoped>
 .container {
-  margin-top: auto;
+  margin: auto;
   text-align: center;
   padding: 20px;
 }
